03ebb3d7b79a17f0a62a4851a0e1a2146258b30f
Jeremy Garnes <j06garnes@gmail.com>
2020-01-17 03:10:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,7 +134,7 @@ def shoe_size(shoes)
     value.each do |attribute, stats|
       if attribute == :players
         stats.each do |player|
-          if player[:player_name] == name
+          if player[:player_name] == shoe
            return player[:shoe]
           end
         end 
