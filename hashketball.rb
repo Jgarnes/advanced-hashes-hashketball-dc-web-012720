11b1c8b03dd5a82b5f3b3d07b01ef713e2177eaa
Jeremy Garnes <j06garnes@gmail.com>
2020-01-17 02:48:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,7 +116,7 @@ stats_hash
 end
 
 def num_points_scored(name)
-  game_hash.each do |key, value|
+  stats_hash.each do |key, value|
     value.each do |key2, stats|
       if key2 == :players
         stats.each do |player|
